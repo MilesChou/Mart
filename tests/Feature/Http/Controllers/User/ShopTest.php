cc7624572a4a4472d255ebbd6a0c3e42699d1494
MilesChou <jangconan@gmail.com>
2023-08-18 06:59:24 +0800

    Add test

--- tests/Feature/Http/Controllers/User/ShopTest.php
+++ tests/Feature/Http/Controllers/User/ShopTest.php
@@ -2,9 +2,7 @@
 
 namespace Tests\Feature\Http\Controllers\User;
 
-use App\Features\Operation;
 use Illuminate\Foundation\Testing\RefreshDatabase;
-use Laravel\Pennant\Feature;
 use PHPUnit\Framework\Attributes\Test;
 use Tests\TestCase;
 
