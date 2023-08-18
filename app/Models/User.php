42a8b8760b6f2b88d00fa6f8df3ec53252ae64ca
MilesChou <jangconan@gmail.com>
2023-08-18 10:40:29 +0800

    Add DB and model file for CustomerQuestion

--- app/Models/User.php
+++ app/Models/User.php
@@ -76,4 +76,9 @@ class User extends Authenticatable
     {
         return $this->hasMany(Order::class);
     }
+
+    public function questions(): HasMany
+    {
+        return $this->hasMany(CustomerQuestion::class);
+    }
 }
