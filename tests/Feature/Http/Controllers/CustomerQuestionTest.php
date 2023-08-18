509554ec4d5f53d0a4d990beea4bc53bd03d0e6c
MilesChou <jangconan@gmail.com>
2023-08-18 11:09:27 +0800

    Add test for CustomerQuestion

--- tests/Feature/Http/Controllers/CustomerQuestionTest.php
+++ tests/Feature/Http/Controllers/CustomerQuestionTest.php
@@ -7,6 +7,7 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Support\Facades\Artisan;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Support\Facades\Auth;
+use Laravel\Pennant\Feature;
 use PHPUnit\Framework\Attributes\Test;
 use Tests\TestCase;
 
@@ -42,9 +43,33 @@ class CustomerQuestionTest extends TestCase
 
         $response = $this->post('/customer-question', [
             'question' => 'Is is good?',
-            'product_id' => 1
+            'product_id' => 1,
         ]);
         $response->assertStatus(302);
         $this->assertCount(1, CustomerQuestion::all());
+    }
+
+    #[Test]
+    public function seeCustomerQuestionWhenFeatureOn()
+    {
+        Artisan::call('db:seed');
+        Auth::loginUsingId(4); //user according to the seeder
+
+        Feature::for(Auth::user())->activate('customer_question');
+
+        $this->get('/user')
+            ->assertSeeText('My queries');
+    }
+
+    #[Test]
+    public function dontSeeCustomerQuestionWhenFeatureOff()
+    {
+        Artisan::call('db:seed');
+        Auth::loginUsingId(4); //user according to the seeder
+
+        Feature::for(Auth::user())->deactivate('customer_question');
+
+        $this->get('/user')
+            ->assertDontSeeText('My queries');
     }
 }
